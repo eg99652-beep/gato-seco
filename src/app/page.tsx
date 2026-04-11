a5d125c84113523c59d4eaccf609fa924756ee6b
Enrique Gonzalez <enriquegonzalez@MacBook-Air-de-Enrique.local>
2026-04-11 16:25:54 -0600

    Agrega AboutUs al page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
         <CategoryBar />
         <FeaturedDuo />
         <ProductGrid />
+        <AboutUs />
         <MemberBanner />
       </main>
       <Footer />
