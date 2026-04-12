b1dedf82474577cfbcb97a3ee07fd89f9bb6cbfe
Enrique Gonzalez <enriquegonzalez@MacBook-Air-de-Enrique.local>
2026-04-11 21:38:05 -0600

    Quitar ProductGrid del home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </section>
         <CategoryBar />
         <FeaturedDuo />
-        <ProductGrid />
+
         <MemberBanner />
       </main>
       <Footer />
