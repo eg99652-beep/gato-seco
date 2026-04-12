f464ddc7cdd589550bb3ed4e3b5a86fa20b7c008
Enrique Gonzalez <enriquegonzalez@MacBook-Air-de-Enrique.local>
2026-04-11 20:01:03 -0600

    Zoom 55%

--- src/components/nike/AboutUs.tsx
+++ src/components/nike/AboutUs.tsx
@@ -23,7 +23,7 @@ export default function AboutUs() {
               alt="Gato Seco logo"
               fill
               className="object-cover"
-              style={{ transform: "scale(0.65)", transformOrigin: "center" }}
+              style={{ transform: "scale(0.55)", transformOrigin: "center" }}
             />
           </div>
           <div className="absolute bottom-0 w-full flex items-end justify-start p-6">
